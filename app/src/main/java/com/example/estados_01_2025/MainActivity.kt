756a890d87c03d2077d11ba350e179e593bfca88
luis jose sanchez carrasco <luissancar@yahoo.es>
2024-09-23 18:00:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/estados_01_2025/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.estados_01_2025
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.estados_01_2025.ui.theme.Estados_01_2025Theme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Estados_01_2025Theme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("App") }
+                        )
+                    },
+
+                   modifier = Modifier.fillMaxSize()
+                ) {
+                    innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Mystate01()
+}
+
+
+@Composable
+fun Mystate01(){
+  //  var contador = 0
+    var contador = remember {mutableStateOf(0)  }
+    Column(Modifier
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Button(onClick =   {contador.value++}) {
+            Text("Button")
+        }
+        Text(text = "Pulsado ${contador.value} veces")
+    }
+
+}
